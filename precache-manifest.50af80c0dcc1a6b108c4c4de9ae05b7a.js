fab655daf4e235481906ab4fe05b7b7bdaa71765
ericktorres <ericktorresdeveloper@gmail.com>
2018-11-04 05:52:39 -0600

    Updates

---
+++ precache-manifest.50af80c0dcc1a6b108c4c4de9ae05b7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca5f54877e61b5fcc794",
+    "url": "/static/js/main.ca5f5487.chunk.js"
+  },
+  {
+    "revision": "3b9d73e7daf064454872",
+    "url": "/static/js/1.3b9d73e7.chunk.js"
+  },
+  {
+    "revision": "ca5f54877e61b5fcc794",
+    "url": "/static/css/main.be1c00c8.chunk.css"
+  },
+  {
+    "revision": "d7a42ba29201ff2fc5a648d487296ab1",
+    "url": "/index.html"
+  }
+];
